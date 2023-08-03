e9c9322e4676790eac7a67b5d56886acc8b5a418
Spolavore <75754941+Spolavore@users.noreply.github.com>
2023-08-02 23:53:07 -0300

    trabalhando na class user

--- User.py
+++ User.py
@@ -6,14 +6,5 @@ class User:
     
     def add_rating(self, player):
         # se o tamanho dos players_rated registrado ainda nao chegou em 20 então só adiciona
-        if len(self.players_rated) < 20:
-            self.players_rated = [*self.players_rated, player]
-        elif len(self.players_rated) >= 20:
-            menor = self.players_rated[0]
-            for i in range(0,len(self.players_rated)):
-                if self.players_rated[i][1] < menor[1]:
-                   menor = self.players_rated[i]
-
-            print(f'Menor: {menor}')
-
-   
+        if player[0] not in self.players_rated:
+            self.players_rated = [*self.players_rated, player[0]]
