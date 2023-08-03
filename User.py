9faca64da7337d82c46baad8134a1e3c107c106b
Spolavore <75754941+Spolavore@users.noreply.github.com>
2023-08-03 12:50:16 -0300

    otimizando o codigo

--- User.py
+++ User.py
@@ -2,9 +2,24 @@ class User:
     def __init__(self, id, next_user=None):
         self.id = id
         self.players_rated = [] # tamanho limitado para 20, pois só ira aparecer os 20 maiores
+        self.top_ratings = [] # notas que o usuário deu pro player ordenadas
+        self.isOrr = False
         self.proximo =  next_user
     
     def add_rating(self, player):
         # se o tamanho dos players_rated registrado ainda nao chegou em 20 então só adiciona
         if player[0] not in self.players_rated:
             self.players_rated = [*self.players_rated, player[0]]
+
+    def get_top_ratings(self):
+        if not self.isOrr:
+            for player in self.players_rated:
+                self.top_ratings.append(player.rating)
+            self.top_ratings.sort(reverse=True)
+            self.isOrr = True
+        
+        return self.top_ratings
+                
+        
+            
+       
--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import time
 from Hash import Hash
 from Trie import Trie
 
+
 if __name__== '__main__':
+    start_time = time.time()
     # pré-processamento #
     jogadores = Hash(10000)
     users = Hash(10000)
@@ -14,31 +17,47 @@ if __name__== '__main__':
             jogadores.add(data, 'Player')
 
     # adicionando a nota e os usuários (essa parte do codigo ta demorando um pouco pra rodar)
-    with open('./tables/minirating.csv') as rating:
+    with open('./tables/rating.csv') as rating:
         next(rating)
+
+        ids_already_searched = []
+        players_already_searched = []
+        index = -1
+
         for linha in rating:
             # pegando e separando informações contidas na linhas
+
             infos = linha.split(',', 3)
             userId = int(infos[0])
             fifaId = int(infos[1])
-            rating = float(infos[2][0:len(infos[2]) -1])
-            
+            rating = (infos[2])
+            pass
             #consultado qual player deve ter a nota alterada
-            player = jogadores.consulta(fifaId)
-            #mudando o rating desse jogador pego
-            player.set_rating(rating)
+            # if fifaId not in ids_already_searched:
+            #     player = jogadores.consulta(fifaId)
+            #     ids_already_searched.append(fifaId)
+            #     players_already_searched.append(player)
+            #     #mudando o rating desse jogador pego
+            #     player.set_rating(rating)
+            #     index += 1
+            # else:
+            #     player = players_already_searched[index]
+            #     player.set_rating(rating)
+
             
             #inserindo usuários
             #busca o usuário na lista
-            user_on_search = users.consulta(userId)
+            # user_on_search = users.consulta(userId)
 
-            # caso ele não esteja ainda na hash adiciona ele
-            if user_on_search == None:
-                users.add(userId, 'User', (player, player.rating))
-            else:
-                # se o user já foi inserido então apenas adiciona a nova nota dada
-                # a determinado jogador na lista de ratings
-                user_on_search.add_rating((player, player.rating))
+            # # caso ele não esteja ainda na hash adiciona ele
+            # if user_on_search == None:
+            #     users.add(userId, 'User', (player, player.rating))
+            # else:
+            #     # se o user já foi inserido então apenas adiciona a nova nota dada
+            #     # a determinado jogador na lista de ratings
+            #     user_on_search.add_rating((player, player.rating))
 
-        print(users.consulta(32010).players_rated)
         #verificar pois nao deveria aparecer tantos elemento
+
+        print(time.time() - start_time, 's')
+    
